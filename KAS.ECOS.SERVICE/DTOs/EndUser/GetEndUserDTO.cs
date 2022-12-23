28832c16a5e6794c4b09cf58e4fe793dd210abf6
unknown <anhkhoa.nguyen@stunited.vn>
2022-12-23 15:31:26 +0700

    Ad ID when return end user

--- KAS.ECOS.SERVICE/DTOs/EndUser/GetEndUserDTO.cs
+++ KAS.ECOS.SERVICE/DTOs/EndUser/GetEndUserDTO.cs
@@ -9,12 +9,14 @@ namespace KAS.ECOS.SERVICE.DTOs.EndUser
 {
     public class GetEndUserDTO
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
         public bool IsActive { get; set; }
+        public string? Type { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime DeletedDate { get; set; }
     }
